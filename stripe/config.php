22ae1df1ba0bd2a44e80ece11f6c266ecac4aba4
Sandaru Gunathilake <maduhansadilshan@gmail.com>
2024-11-26 12:27:29 +0530

    Update config.php

--- stripe/config.php
+++ stripe/config.php
@@ -2,8 +2,8 @@
     require_once "stripe-php-master/init.php";
 
     $stripeDetails = array(
-        "secretKey" => "sk_test_51LUtaCLtfP1LafvhvuZF8xMBC7XoXIXUE8BfumMA3O8fbipMCad50ga0WxcgxIHGx6fYYiezulfus5AsLCUqTQqz00S6xksDUJ",
-        "publishableKey" => "pk_test_51LUtaCLtfP1Lafvh8p1kssA6WnpXQ7ej4Stwz5cZvpojQp55xQQWjWToyXjR2671EYcXb4Uov6dDJaf341mjLHkt00R1Tiwte2"
+        "secretKey" => "YOUR_SECRET_KEY",
+        "publishableKey" => "YOUR_PUBLISHABLE_KEY"
     );
 
     \Stripe\Stripe::setApiKey($stripeDetails["secretKey"]);
